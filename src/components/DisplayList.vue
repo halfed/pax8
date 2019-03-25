955da6451ec413a2ca3332edfb111aa920ddfcf8
Ed Wince <1half.ed@gmail.com>
2019-03-24 23:28:16 -0600

    Create DisplayList.vue

---
+++ src/components/DisplayList.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<ul v-for="company in companyList" :key="company.id">
+			<li>{{ company.id }}</li>
+			<li>{{ company.name }}</li>
+			<li><router-link :to="'/edit-company/'+company.id">Edit</router-link></li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'DisplayList',
+	props: ['companyList'],
+	data() {
+		return {
+			company: ''
+		}
+	}
+}
+</script>
