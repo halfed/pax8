63dfd3b631dcdaffb3ef1afe7fcd1ef031341701
Ed Wince <1half.ed@gmail.com>
2019-03-24 23:26:09 -0600

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<h1>This is the home page</h1>
+  		<div>
+  			<display-list :companyList="companies"/>
+  			
+  		</div>
+	</div>
+  
+  
+</template>
+
+<script>
+import DisplayList from '../components/DisplayList.vue';
+import GetAll from '../services/CompanyService';
+export default {
+  name: 'home',
+  components: {
+  	DisplayList
+  },
+  data() {
+  	return {
+  		companies: []
+  	}
+  },
+  created() {
+  	this.getCompanies();
+  },
+  methods: {
+  	getCompanies() {
+  		const promise = GetAll.getAll()
+  		.then(companies => {
+  			this.companies = companies;
+  		});
+  	}
+  }
+};
+</script>
